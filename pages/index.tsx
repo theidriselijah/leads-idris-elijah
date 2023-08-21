431be9bc6beb3731bdb873383a35d611ab660095
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-08-20 18:26:55 -0700

    Implmented google busines profile lead magnet

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,46 @@ import Image from 'next/image'
 import Head from 'next/head'
 import { Link } from 'react-scroll'
 import { InlineWidget } from 'react-calendly'
+import { useState } from 'react'
 
 export default function Home() {
+  const initialFormData = {
+    email: '',
+    message: '',
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  function handleChange(event: any) {
+    const { name, value } = event.target
+
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      }
+    })
+  }
+
+  async function subscribeUser(event: any) {
+    event.preventDefault()
+    const res = await fetch('/api/convertkitSubscribe', {
+      body: JSON.stringify({ email: formData.email }),
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      method: 'POST',
+    })
+
+    const json_res = await res.json()
+
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        message: json_res.message,
+        email: '',
+      }
+    })
+  }
+
   return (
     <main>
       <Head>
@@ -52,18 +90,38 @@ export default function Home() {
               </span>{' '}
               for maximum visibility. Totally free - no strings attached.
             </p>
-            <form className="mb-[20px]">
+
+            <form onSubmit={subscribeUser} className="mb-[20px]">
               <input
+                id="email"
                 type="email"
                 placeholder="Enter Your Email"
                 name="email"
-                className="bg-[#FCFCFC] text-center rounded w-full h-[39px] mb-[20px] text-[15px]"
+                onChange={handleChange}
+                value={formData.email}
+                required
+                autoComplete="on"
+                className="bg-[#FCFCFC] text-[#1E2D44] text-center rounded w-full h-[39px] mb-[20px] text-[15px]"
               />
-              <button className="secondary-button w-[258px] h-[42px] text-[17px]">
+              <button
+                type="submit"
+                name="email"
+                className="secondary-button w-[258px] h-[42px] text-[17px]"
+              >
                 Claim Your Free Setup
               </button>
+              <div className='flex justify-center'>
+                {formData.message.length > 0 && (
+                  <p
+                    className="bg-[#698C40] text-center text-sm mt-3 w-[300px] rounded-[5px] p-2"
+                  >
+                    {formData.message}
+                  </p>
+                )}
+              </div>
             </form>
-            <p className="">
+
+            <p className="text-[14px]">
               I&apos;ll contact you within one business day to complete your
               setup.
             </p>
