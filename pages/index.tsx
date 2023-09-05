3100902b47361ce0f00736cc0ba823e3670fff13
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-09-05 13:09:10 -0700

    improved responsiveness of hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,11 +96,11 @@ export default function Home() {
       </nav>
 
       {/* Hero section */}
-      <section className="bg-[url('/assets/hero-image.webp')] bg-[#1E2D44] bg-opacity-80 bg-blend-overlay bg-[center_top_-13.5rem] bg-no-repeat w-full h-[422px] flex flex-col justify-center items-center text-center text-[#FFFFFF]">
-        <h1 className="text-[36px] md:text-[60px] max-w-2xl text-[#FF9801] font-bold">
+      <section className="bg-[url('/assets/hero-image.webp')] bg-[#1E2D44] bg-opacity-80 bg-blend-overlay bg-[center_top_-13.5rem] bg-no-repeat w-full h-[422px] flex flex-col justify-center items-center text-center text-[#FFFFFF] px-[20px]">
+        <h1 className="text-[36px] md:text-[60px] md:max-w-2xl text-[#FF9801] font-bold">
           Lead Generation For Local Businesses
         </h1>
-        <p className="text-[24px] md:text-[38px] max-w-md">
+        <p className="text-[24px] max-w-xs md:text-[38px] md:max-w-md">
           We build the bridge from click to client.
         </p>
       </section>
