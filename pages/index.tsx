d087a4b741f9e0eb75e86263fffded58195407c1
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-09-06 13:55:38 -0700

    Removed Hints, impacted performance significantly

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,19 +51,6 @@ export default function Home() {
           content="I specialize in lead generation for business owners looking for local leads. I cover everything from web design, search engine optimization, email list building, and your social media presence to make sure you have a pipeline of well-qualified leads. Most of my clients aren't sure what online strategies are working the best right now, and even if they knew what to do, they don't have the time to do it. If you're a business owner looking for local leads but already feel stretched so thin you might snap, we should talk."
         />
         <link rel="icon" href="/assets/favicon-32x32.png" sizes="any" />
-        <link
-          rel="preconnect"
-          href="https://www.gstatic.com/"
-        />
-        <link
-          rel="preconnect"
-          href="https://assets.calendly.com/"
-        />
-        <link rel="preconnect" href="https://www.googletagmanager.com/" />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
