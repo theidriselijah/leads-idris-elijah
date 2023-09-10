4b2d5db0854e45a9162db62ec90548c382445900
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-09-10 16:15:30 -0700

    Add Google Analytics, and Calendly inline widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,12 @@ import { Link } from 'react-scroll'
 import NavArea from '@/components/NavArea'
 import HeroSection from '@/components/HeroSection'
 import GoogleSetupForm from '@/components/GoogleSetupForm'
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 
-// const DynamicCalendly = dynamic(() => import('../components/CalendlyInlineWidget'), {
-//   ssr: false,
-// })
+const DynamicCalendly = dynamic(() => import('../components/CalendlyInlineWidget'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+})
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
           content="I specialize in lead generation for business owners looking for local leads. I cover everything from web design, search engine optimization, email list building, and your social media presence to make sure you have a pipeline of well-qualified leads. Most of my clients aren't sure what online strategies are working the best right now, and even if they knew what to do, they don't have the time to do it. If you're a business owner looking for local leads but already feel stretched so thin you might snap, we should talk."
         />
         <link rel="icon" href="/assets/favicon-32x32.png" sizes="any" />
-        {/* <script
+        <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
         />
@@ -40,7 +41,7 @@ export default function Home() {
           defer={true}
           type="text/javascript"
           src="https://assets.calendly.com/assets/external/widget.js"
-        ></script> */}
+        ></script>
       </Head>
 
       {/* Navigation area */}
@@ -538,7 +539,7 @@ export default function Home() {
 
         {/* Calendly inline widget */}
         <div>
-          {/* <DynamicCalendly /> */}
+          <DynamicCalendly />
         </div>
       </section>
     </main>
